261b1b17d794d12a72e368d5096948cd28ed39ba
Gulbanu <gulbanu140313@gmail.com>
2025-01-02 19:26:07 +0300

    Add files via upload

--- js/task-3.js
+++ js/task-3.js
@@ -4,7 +4,7 @@ function getElementWidth(content, padding, border) {
     const borderValue = parseFloat(border) * 2;
     
     const totalWidth = contentValue + paddingValue + borderValue;
-    return `${totalWidth}px`;
+    return totalWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
